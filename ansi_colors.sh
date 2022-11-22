6852e64e5bd5ea3b4ec58b41c7a99b322c1ac6c6
komeno <tkomeno@student.42tokyo.jp>
2022-11-21 21:30:30 -0300

    added ansi colors

---
+++ ansi_colors.sh
@@ -0,0 +1,37 @@
+# Styles.
+Normal="\x1b[0m"
+Bold="\x1b[1m"
+Faint="\x1b[2m"
+Italic="\x1b[3m"
+Underline="\x1b[4m"
+Blink_Slow="\x1b[5m"
+Blink_Rapid="\x1b[6m"
+Inverse="\x1b[7m"
+Conceal="\x1b[8m"
+Crossed_Out="\x1b[9m"
+
+# Text colors.
+Black="\x1b[30m"
+Red="\x1b[31m"
+Green="\x1b[32m"
+Yellow="\x1b[33m"
+Blue="\x1b[34m"
+Magenta="\x1b[35m"
+Cyan="\x1b[36m"
+White="\x1b[37m"
+
+# Background colors.
+Bg_Black="\x1b[40m"
+Bg_Red="\x1b[41m"
+Bg_Green="\x1b[42m"
+Bg_Yellow="\x1b[43m"
+Bg_Blue="\x1b[44m"
+Bg_Magenta="\x1b[45m"
+Bg_Cyan="\x1b[46m"
+Bg_White="\x1b[47m"
+
+# Resets
+NoStyle="\x1b[0m"
+NoUnderline="\x1b[24m"
+NoInverse="\x1b[27m"
+NoColor="\x1b[39m"
